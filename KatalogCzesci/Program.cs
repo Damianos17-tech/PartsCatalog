8ed9d8e6e4d3cc705b7d2c6a06acc88edc505be3
Damianos <236268699+Damianos17-tech@users.noreply.github.com>
2026-08-25 23:27:40 +0200

    Logging 1 step

--- KatalogCzesci/Program.cs
+++ KatalogCzesci/Program.cs
@@ -1,42 +1,141 @@
 using KatalogCzesci.Components;
 using KatalogCzesci.Services;
-//using System.Diagnostics;
-
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// =================================================
+// SERVICES
+// =================================================
 
 builder.Services.AddScoped<CatalogService>();
 builder.Services.AddScoped<AdSearchService>();
 
 
-// Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+// =================================================
+// AUTHENTICATION
+// =================================================
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/access-denied";
+    });
+
+builder.Services.AddAuthorization();
+
+
+// =================================================
+// BLAZOR
+// =================================================
+
+builder.Services
+    .AddRazorComponents()
+    .AddInteractiveServerComponents();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+// =================================================
+// HTTP REQUEST PIPELINE
+// =================================================
+
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler(
+        "/Error",
+        createScopeForErrors: true);
+
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+
+
+// =================================================
+// AUTHENTICATION / AUTHORIZATION
+// =================================================
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+// =================================================
+// ANTIFORGERY
+// =================================================
+
 app.UseAntiforgery();
+
+
+// =================================================
+// BLAZOR
+// =================================================
+
+
+
+app.MapPost("/api/login", async (HttpContext context) =>
+{
+    var data =
+        await System.Text.Json.JsonSerializer
+            .DeserializeAsync<LoginRequest>(
+                context.Request.Body);
+
+    Console.WriteLine(
+    $"LOGIN TEST: [{data?.Username}] / [{data?.Password}]");
+
+    if (data?.Username == "Andrzej" &&
+        data.Password == "test123")
+    {
+        var claims = new[]
+        {
+            new System.Security.Claims.Claim(
+                System.Security.Claims.ClaimTypes.Name,
+                "Andrzej")
+        };
+
+        var identity =
+            new System.Security.Claims.ClaimsIdentity(
+                claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var principal =
+            new System.Security.Claims.ClaimsPrincipal(identity);
+
+        await context.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            principal);
+
+        return Results.Ok();
+    }
+
+    return Results.Unauthorized();
+})
+.DisableAntiforgery();
+
+
+
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-//app.Lifetime.ApplicationStarted.Register(() =>
-//{
-//    Process.Start(new ProcessStartInfo
-//    {
-//        FileName = "http://localhost:5000",
-//        UseShellExecute = true
-//    });
-//});
+
+// =================================================
+// RUN
+// =================================================
 
 app.Run();
+
+
+// =================================================
+// LOGIN MODEL
+// =================================================
+
+public record LoginRequest(
+    string Username,
+    string Password);
---
+++ KatalogCzesci/wwwroot/js/login.js
@@ -0,0 +1,20 @@
+﻿window.loginUser = async function (username, password) {
+
+    console.log("JS USER:", username);
+    console.log("JS PASSWORD:", password);
+
+    const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            Username: username,
+            Password: password
+        })
+    });
+
+    console.log("HTTP STATUS:", response.status);
+
+    return response.ok;
+};
